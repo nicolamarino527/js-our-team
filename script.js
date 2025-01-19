5b42fbd585656b4d679acb948d67abc0ad29e2d5
nicolamarino <nicolamarino527@gmail.com>
2025-01-19 21:33:52 +0100

    add script

--- script.js
+++ script.js
@@ -37,3 +37,35 @@ const teamMembers = [
   }
 ];
 
+// Segniamo le variabili
+const cards = document.getElementById("cards");
+let items= '';
+
+// Facciamo un ciclo per prendere gli elementi dall'array
+for (let i = 0; i < teamMembers.length; i++) {
+  let membroIesimo = teamMembers[i];  
+  const {name, role, email, img} = membroIesimo;
+  
+  // Creiamo la lista da aggiungere
+  items +=
+  `<div class="card mb-3 bg-dark" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${img}" class="img-fluid rounded-start" alt="Image of ${name}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title text-white">${name}</h5>
+              <p class="card-text text-white">${role}</p>
+              <p class="card-text text-primary">${email}<small class="text-body-secondary"></small></p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+}
+
+// Aggiungiamo gli elementi alla pagina
+cards.innerHTML = items;
+console.log(items);
+console.log(membroIesimo);
+
